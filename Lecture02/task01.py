39736fc3b0910cb32ca7610082859625cdcf29ba
muathothman <muath.othman@gmail.com>
2025-09-02 11:15:23 +0300

    I have done task 01 in lecutre02

--- Lecture02/task01.py
+++ Lecture02/task01.py
@@ -1,1 +1,10 @@
-print("Othman")
+total_coins = 0
+
+coins = int(input("Give me money: "))
+total_coins += coins
+
+while total_coins <= 5:
+    print(f"Not enough! Give me {5-total_coins}")
+    coins = int(input("Add more money: "))
+    total_coins += coins
+print("Thank You!")
